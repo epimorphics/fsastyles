50e777074851ac0e52183f0fbd93b3fe30e0637d
joescottdave <joseph.davies@outlook.com>
2020-11-17 14:33:45 +0100

    Workaround bug with postcss-nested and postcss-mixins
    Caused by a bug in postcss-nested v7 using postcss v8. Nested media
    queries compiled from a mixin where not being un-nested. By reverting to
    postcss 7 and previous versions of some plugins this commit appears to
    fix the problem.

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-mixins': {},
     'postcss-custom-media': {},
     'postcss-custom-selectors': {},
+    'postcss-mixins': {},
     'postcss-nested': {},
-    'postcss-normalize': {},
-    'postcss-discard-comments': {},
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    }
+    },
+    'postcss-normalize': {},
+    'postcss-discard-comments': {}
   }
 }
